9e8cf86cac84ab07824fee03b10f99ed65825d45
KseniiaAlekseeva <csushaalekseeva@gmail.com>
2023-04-17 21:45:22 +0300

    Task19: correct pop

--- task19.py
+++ task19.py
@@ -3,7 +3,7 @@ nums = [1, 2, 3, 4, 5, 6, 7]
 sdvig = int(input("Enter k: ")) % len(nums)
 
 # for _ in range(sdvig):
-#     nums.insert(0, nums.pop(len(nums)-1))
+#     nums.insert(0, nums.pop())
 # print(nums)
 
 nums = nums[-sdvig:]+nums[:-sdvig]
